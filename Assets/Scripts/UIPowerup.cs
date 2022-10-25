a7d7ebb28f9a6fe254a1619330c90a2476bab668
dtolomelli <daniele.tolomelli@vection.com.au>
2022-10-25 23:23:50 +0200

    added ui powerup with powerup tooltip

---
+++ Assets/Scripts/UIPowerup.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPowerup : MonoBehaviour
+{
+    [SerializeField] PowerupTooltip tooltipPrefab;
+    [SerializeField] Transform parent;
+
+    PowerupTooltip tooltip;
+    public string powerupMessage;
+
+    public float tooltipOffset { get => transform.position.x < 0.5f * Screen.width ? 200f : -50f ; }
+
+    void Start()
+    {
+        if (transform.position.x > 0.5f * Screen.width)
+            parent.transform.localScale = new Vector3(-1f, 1f, 1f);
+    }
+
+    public void OnPointerEnter()
+    {
+        tooltip = Instantiate(tooltipPrefab, transform.parent);
+        tooltip.Init(powerupMessage, tooltipOffset);
+        tooltip.transform.position = Input.mousePosition;
+    }
+
+    public void OnPointerExit()
+    {
+        if (tooltip == null)
+            return;
+            
+        Destroy(tooltip.gameObject);
+    }
+}
