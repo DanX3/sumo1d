bf463c748694fda443375c8e4691c0c2a89702d8
dtolomelli <daniele.tolomelli@vection.com.au>
2022-11-01 14:15:29 +0100

    Added Player arena indicator

---
+++ Assets/Scripts/UIArena.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class UIArena : MonoBehaviour
+{
+
+    int hp;
+    int startHp;
+    float startSizeX;
+
+    public void Init(int hp)
+    {
+        startHp = this.hp = hp;
+        startSizeX = GetComponent<RectTransform>().sizeDelta.x;
+    }
+
+    public void AddDiff(int diff)
+    {
+        hp += diff;
+        float scaleFactor = (float)hp / startHp;
+        float targetSize = startSizeX * scaleFactor;
+        GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, targetSize);
+    }
+
+    void Start()
+    {
+        StartCoroutine(Tester());
+    }
+
+    IEnumerator Tester()
+    {
+        Init(100); yield return new WaitForSeconds(2f);
+        AddDiff(10); yield return new WaitForSeconds(2f);
+        AddDiff(10); yield return new WaitForSeconds(2f);
+        AddDiff(-20); yield return new WaitForSeconds(2f);
+        AddDiff(-50); yield return new WaitForSeconds(2f);
+    }
+
+}
