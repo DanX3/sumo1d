683c86df2728d695f84298f273b44e6e77819a5c
dtolomelli <daniele.tolomelli@vection.com.au>
2022-11-06 18:03:13 +0100

    Added range to PlayerAttributes

--- Assets/Scripts/Players/PlayerAttributes.cs
+++ Assets/Scripts/Players/PlayerAttributes.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerAttributes
 {
+    [Range(1, 9)]
     public int power;
+    [Range(1, 9)]
     public int spirit;
+    [Range(1, 9)]
     public int weight;
+    [Range(1, 9)]
     public int reflex;
+    [Range(1, 9)]
     public int critical;
 }
