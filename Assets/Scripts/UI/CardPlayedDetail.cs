bbc65c548713614563904b58578c3de4fe22c806
Daniele Ferrari <daniele.ferrari.work@gmail.com>
2022-11-10 23:51:51 +0100

    fix card detail position

--- Assets/Scripts/UI/CardPlayedDetail.cs
+++ Assets/Scripts/UI/CardPlayedDetail.cs
@@ -26,6 +26,9 @@ public class CardPlayedDetail : MonoBehaviour
         showingCardTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalTransform.rect.width);
         showingCardTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, originalTransform.rect.height);
 
+        showingCardTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        showingCardTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        showingCardTransform.pivot = new Vector2(0.5f, 0.5f);
 
         StartCoroutine(RemoveShowingCard());
     }
